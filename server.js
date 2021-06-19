3b9be877fa072ad91308a2812864b7075032a4a3
flg <pluton3@icloud.com>
2021-06-19 13:11:22 +0200

    aggiunte API (PUT e DELETE) e validazione

--- server.js
+++ server.js
@@ -3,14 +3,17 @@
 // server express
 const express = require('express');
 
+// dayjs
+const dayjs = require('dayjs');
+
 // morgan, per il logging (middleware)
 const morgan = require('morgan');
 
 // Validator
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 // modulo per accedere al db
-const dao = require('./dao');
+const examDao = require('./exam-dao');
 
 // init express
 const app = express();
@@ -35,47 +38,60 @@ app.use(express.json());
 
 // GET /api/courses
 app.get('/api/courses', async (req, res) => {
-    try{
-    const courses = await dao.listCourses();
-    res.status(200).json(courses);
+    try {
+        const courses = await dao.listCourses();
+        res.status(200).json(courses);
     } catch (err) {
-        res.status(500).end();
+        res.status(500).json({ error: 'Errore DB' });
     }
 });
 
 // GET /api/exams
 app.get('/api/exams', async (req, res) => {
-    dao.listExams()
+    examDao.listExams()
         .then((exams) => res.json(exams))
-        .catch((err) => res.status(500).json({error: 'Errore DB'}));
+        .catch((err) => res.status(500).json({ error: 'Errore DB' }));
 });
 
-// GET /api/courses
-app.get('/api/courses/:code', async (req, res) => {
-    try{
-    // ...MANCA VALIDAZIONE...
-    const result = await dao.getCourse(req.params.code);
-    if (result.error)
-        res.status(404).json(result);
-    else 
-        res.status(200).json(result);
+// GET /api/courses/:code
+app.get('/api/courses/:code', [check('code').isLength({ min: 7, max: 7 }).matches(/\d\d[A-Z]{5}/)], async (req, res) => {
+    try {
+        // VALIDAZIONE
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        // QUERY DB
+        const result = await examDao.getCourse(req.params.code);
+        if (result.error)
+            res.status(404).json(result); // cerca ma riceve stringa vuota, restituisce oggetto {error: ...}
+        else
+            res.status(200).json(result);
     } catch (err) {
-        res.status(500).json({error: 'Errore DB'});
+        res.status(500).json({ error: 'Errore DB' });
     }
 });
 
 // POST /api/exams
 app.post('/api/exams', [
-    check('score').isInt({min: 18, max: 31}), 
-    check('code').isLength({min: 7, max: 7}),
-    check('date').isDate({format: 'YYYY-MM-DD', strictMode: true})
+    check('score').isInt({ min: 18, max: 31 }),
+    check('code').isLength({ min: 7, max: 7 }),
+    check('date').isDate({ format: 'YYYY-MM-DD', strictMode: true })
 ], (req, res) => {
-    
+
     const errors = validationResult(req);
-   
-    if (!errors.isEmpty()){
-        return res.status(422).json({errors: errors.array()})
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
     }
+
+    const date = dayjs(req.body.date);
+
+    if (!date.isBefore())
+        return res.status(400).json({ error: `La data ${date.format('DD/MM/YYYY')} non è valida.` });
+
     const exam = {
         code: req.body.code,
         score: req.body.score,
@@ -85,22 +101,66 @@ app.post('/api/exams', [
     //await dao.createExam(exam);
     //res.status(201).end();
 
-    dao.createExam(exam)
-    .then( () => res.status(201).end() )
-    .catch( (err) => res.status(500).json({error: 'DB Error'}));
-    
+    examDao.createExam(exam)
+        .then(() => res.status(201).end())
+        .catch((err) => res.status(500).json({ error: `DB Error during the creation of exam ${exam.code}` }));
+
 });
 
-// http://localhost:3001/api/exams
-// {"code": "abc", "date": "2021-05-06", "score": "31"}
-
 // PUT /api/exams
-//app.put('', (req, res) => {});
+app.put('/api/exams', [
+    check('score').isInt({ min: 18, max: 31 }),
+    check('code').isLength({ min: 7, max: 7 }),
+    check('date').isDate({ format: 'YYYY-MM-DD', strictMode: true })
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+    }
+
+    const exam = {
+        code: req.body.code,
+        score: req.body.score,
+        date: req.body.date
+    };
+
+    const date = dayjs(req.body.date);
+
+    if (!date.isBefore())
+        return res.status(400).json({ error: `La data ${date.format('DD/MM/YYYY')} non è valida.` });
+
+    examDao.updateExam(exam)
+        .then((id) => res.status(200).json({ ID: id, message: `Exam ${req.body.code} updated.` }))
+        .catch((err) => res.status(500).json({ error: `DB Error during the update of exam ${exam.code}` }))
+
+});
 
 // DELETE /api/exams/:code
-//app.delete('/api/exams/:code', (req, res) => {});
+app.delete('/api/exams/:code', [
+    check('code').isLength({ min: 7, max: 7 }).matches(/\d\d[A-Z]{5}/)],
+    (req, res) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        examDao.deleteExam(req.params.code)
+            .then(() => res.status(200).json({ message: `Exam ${req.params.code} deleted.` }))
+            .catch((err) => res.status(500).json({ error: 'DB error' }));
+
+    });
 
 // Attivare il server
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
+/* ----------------- FINE ---------------*/
+
+// STRINGA PROVA
+// http://localhost:3001/api/exams
+// {"code": "abc", "date": "2021-05-06", "score": "31"}
